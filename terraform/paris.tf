a6dffd6848e4448779950154e522114182c0c044
Nadav <nadavops@protonmail.com>
2019-02-09 11:05:23 +0200

    setting basics

--- terraform/paris.tf
+++ terraform/paris.tf
@@ -24,7 +24,7 @@ variable "RHEL_image" {default = "ami-5026902d"}
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-east-3"
+  region     = "eu-west-3"
 }
 
 ##################################################################################
