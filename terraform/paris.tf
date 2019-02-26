42d5ff1ac66b33a7331485c3b4d1f143350710a8
Nadav <nadavops@protonmail.com>
2019-02-26 18:25:52 +0200

    remove jenkins pass

--- terraform/paris.tf
+++ terraform/paris.tf
@@ -106,7 +106,51 @@ resource "aws_iam_role_policy" "Jenkins_IAM_Policy" {
   name = "Jenkins-Describe-Policy"
   role = "${aws_iam_role.Jenkins_IAM_Role.id}"
 
-  policy = arn:aws:iam::aws:policy/AmazonEC2FullAccess
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "autoscaling.amazonaws.com",
+                        "ec2scheduled.amazonaws.com",
+                        "elasticloadbalancing.amazonaws.com",
+                        "spot.amazonaws.com",
+                        "spotfleet.amazonaws.com",
+                        "transitgateway.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
+}
+EOF
+}
 
 resource "aws_iam_role" "Jenkins_IAM_Role" {
   name = "Jenkins-Role"
