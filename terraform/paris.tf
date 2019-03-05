42bdea52841171cd399ae2d682d9719d7a873a3e
Nadav <nadavops@protonmail.com>
2019-03-05 12:06:51 +0200

    fileabeat + sql

--- terraform/paris.tf
+++ terraform/paris.tf
@@ -406,7 +406,7 @@ resource "aws_instance" "Jenkins_Server" {
 
 resource "aws_instance" "Jenkins_Slave" {
 	ami           = "${var.ubuntu_image_18-04}"
-	instance_type = "t2.small"
+	instance_type = "t2.micro"
 	key_name        = "${var.aws_key_name}"
 	subnet_id = "${aws_subnet.Subnet_main.id}"
 	vpc_security_group_ids = ["${aws_security_group.SecurityGroup_main.id}"]
